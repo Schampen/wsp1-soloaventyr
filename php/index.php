3f413270b115922b154df735a444620932d127b5
Schampen <hampus.mann@elev.ga.ntig.se>
2019-10-10 10:03:11 +0200

    Börjat design

--- php/index.php
+++ php/index.php
@@ -1,18 +1,31 @@
 <?php
-
+session_start();
 include 'include/db.php';
+
+$filteredId = 1;
+
+if(isset($_GET['id'])) {
+    $filteredId = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+    $_SESSION['id'] = $filteredId;
+} elseif(isset($_SESSION['id'])) {
+    $filteredId = filter_var($_SESSION['id'], FILTER_VALIDATE_INT);
+}
+
+
 $sth = $dbh->prepare('SELECT * FROM story
-                    WHERE id = 1');
+                    WHERE id = :filteredId');
+$sth->bindParam(':filteredId', $filteredId);
 $sth->execute();
 $story = $sth->fetch(PDO::FETCH_ASSOC);
 
 $sth = $dbh->prepare('SELECT * FROM links
-                    WHERE story_id = 1');
+                    WHERE story_id = :filteredId');
+$sth->bindParam(':filteredId', $filteredId);
 $sth->execute();
 $links = $sth->fetchAll(PDO::FETCH_ASSOC);
 
 $page_title = "Soloäventyr - Start";
 
-include 'views/index.php';
+include 'views/index_layout.php';
 
 ?>
